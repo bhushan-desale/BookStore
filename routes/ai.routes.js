d033d0f06a093b28f67cda5c60b6f7e08507c5d3
Bhushan Desale <desalebhushan11@gmail.com>
2025-11-08 18:07:36 +0530

    initial commit
    commit changes

---
+++ routes/ai.routes.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import {getBookInsights} from "../controller/ai.controller.js";
+
+const router = express.Router();
+
+
+router.get("/:bookId", protectRoute, getBookInsights )
+export default router;
